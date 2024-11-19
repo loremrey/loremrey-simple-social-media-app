43135ac9374969725b5cc97dc8c951ef0d38137d
muhananaufal <muhananaufal8@gmail.com>
2024-11-19 22:55:17 +0700

    innitial commit

--- app/api/auth/creation/route.ts
+++ app/api/auth/creation/route.ts
@@ -30,5 +30,5 @@ export async function GET() {
 		});
 	}
 
-	return NextResponse.redirect('http://localhost:3000');
+	return NextResponse.redirect(process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://simple-social-media-app-nextjs.vercel.app');
 }
